bcf72cd44c1a41eb5a70437cc6a10e7873217fe4
Qijing <q.wang0220222@arts.ac.uk>
2024-10-07 15:17:31 +0100

    Added favicon to _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import '../css/main.css';
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return 
+    <>
+    <Head>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />;
+    </>
 }
